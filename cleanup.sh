52be736e61457fe15e23b81bd6653dd4c77bc8fd
Abhishek Mekanagadde Vijaya Kumar <mekanagaddevijaya.a@husky.neu.edu>
2020-03-25 22:30:20 -0400

    removing target

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,7 @@
 
 #--------------------------------------------------------------------------------------------
 # To remove all the images
-docker rmi -f $(docker images -q)
+# docker rmi -f $(docker images -q)
+
+docker stop mysqldb
+docker rm mysqldb
